fca5c8a2f82c77043ac52d6048997c1d550e17dc
nihangonenc <nnurgonenc@gmail.com>
2024-10-14 17:11:50 +0300

    Değişikliklerin açıklaması

---
+++ Frontend/Rise/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./components/Home/Home";
+import Logo from "./components/Home/Logo/Logo";
+
+function App() {
+  return (
+    <>
+      <Logo />
+      <Home />
+    </>
+  );
+}
+
+export default App;
