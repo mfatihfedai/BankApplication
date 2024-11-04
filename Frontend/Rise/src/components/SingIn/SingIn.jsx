3ad6b9c37ef6dd3a6edce6ba11a8fdd06a18ae6d
nihangonenc <nnurgonenc@gmail.com>
2024-11-04 11:03:06 +0300

    SingIn started

--- Frontend/Rise/src/components/SingIn/SingIn.jsx
+++ Frontend/Rise/src/components/SingIn/SingIn.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Link,
+} from "@mui/material";
 
-function SingIn() {
+const SingIn = () => {
   return (
-    <div>SingIn</div>
-  )
-}
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          fontFamily: "Montserrat",
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            fontFamily: "Montserrat",
+          }}
+        >
+          İnternet Şubemize Hoş Geldiniz
+        </Typography>
 
-export default SingIn
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="customerNumber"
+            label="T.C. Kimlik/Müşteri Numarası"
+            name="customerNumber"
+            autoComplete="customer-number"
+            autoFocus
+            InputLabelProps={{
+              style: { color: "var(--color-blue)", fontFamily: "Montserrat" }, // Label rengi ve fontu
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": {
+                  borderColor: "var(--color-blue)", // Hover
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "var(--color-blue)", // Focus
+                },
+              },
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Şifre"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            InputLabelProps={{
+              style: { color: "var(--color-blue)", fontFamily: "Montserrat" }, // Label rengi ve fontu
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": {
+                  borderColor: "var(--color-blue)", // Hover
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "var(--color-blue)", // Focus
+                },
+              },
+            }}
+          />
+          <Box>
+            <Link
+              href="#"
+              variant="body2"
+              sx={{
+                color: "var(--color-blue)",
+                fontFamily: "Montserrat",
+              }}
+            >
+              Şifremi Unuttum
+            </Link>
+          </Box>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              fontFamily: "Montserrat",
+              backgroundColor: "var(--color-blue)",
+              color: "var(--color-white)",
+              "&:hover": {
+                backgroundColor: "var(--color-white)",
+                color: "var(--color-blue)",
+              },
+            }}
+          >
+            GİRİŞ YAP
+          </Button>
+          <Box>
+            <Link
+              href="#"
+              variant="body2"
+              sx={{
+                color: "var(--color-blue)",
+                fontFamily: "Montserrat",
+              }}
+            >
+              Müşteri Olmak İster Misiniz?
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+export default SingIn;
