a8eb82f2d8fba1738877edf3dc8d928765a327cc
mfatihfedai <mfatihfedai1@gmail.com>
2024-11-12 16:15:54 +0300

    receipt restapis gönderildi

--- Backend/src/main/java/com/softwareProject/banksApplication/core/Config/RestApis.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/core/Config/RestApis.java
@@ -8,6 +8,7 @@ public class RestApis {
 
     public static final String BANKSSERVICE = DEVELOPER+VERSIONS+"/banks";
     public static final String INVOICESERVICE = DEVELOPER+VERSIONS+"/invoices";
+    public static final String RECEIPTSERVICE = DEVELOPER+VERSIONS+"/receipt";
     public static final String AUTHSERVICE = "/auth";
 
 
