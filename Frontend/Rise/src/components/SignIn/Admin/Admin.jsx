0f7c364b0e2731f0a1276a2141f009253099aa38
nihangonenc <nnurgonenc@gmail.com>
2024-11-14 02:13:20 +0300

    new file added

---
+++ Frontend/Rise/src/components/SignIn/Admin/Admin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Admin() {
+  return <div>Admin Paneline Hoş Geldiniz</div>;
+}
+
+export default Admin;
---
+++ Frontend/Rise/src/components/SignIn/User/User.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function User() {
+  return <div>User sayfasına hoş geldiniz</div>;
+}
+
+export default User;
