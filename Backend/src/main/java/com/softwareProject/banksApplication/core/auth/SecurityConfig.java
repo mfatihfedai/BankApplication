821a51c680790370fdf68ce572216e17ce5f08cc
mfatihfedai <mfatihfedai1@gmail.com>
2024-11-21 13:14:56 +0300

    ufak değişiklikler yapıldı

--- Backend/src/main/java/com/softwareProject/banksApplication/core/auth/SecurityConfig.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/core/auth/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.softwareProject.banksApplication.entity.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
