2b1472dcbf652e226281f14db2cb3307fc5f33e5
mfatihfedai <mfatihfedai1@gmail.com>
2024-12-02 00:17:05 +0300

    frontend ile giriş problemi düzeltildi.

--- Frontend/Rise/src/components/SignIn/Dashboard.jsx
+++ Frontend/Rise/src/components/SignIn/Dashboard.jsx
@@ -5,34 +5,32 @@ function Dashboard() {
 
     const [bank, setBank] = useState([]);
 
-
    useEffect(()=> {
     async function fetchData(){
             try{
               const banks = await getBanks();
               console.log(banks);
-              setBank(banks.items);
-              console.log(bank)
+              setBank(banks.data.items);
+              console.log(bank);
             }catch(err){
               console.log(err.message);
             }
           }
           fetchData();
    },[])
-
     return (
       <div>
       {/* Eğer bank boşsa bir mesaj gösterebilirsiniz */}
-      {/* {bank.length === 0 ? (
+      {bank.length === 0 ? (
         <p>Loading banks...</p>
       ) : (
         <div>
           banks:
-          {bank.map((item, index) => (
-            <p key={index}>{item.name}</p>
+          {bank?.map((item, index) => (
+            <p key={index}>{item.bankName} : {item.transferFee}</p>
           ))}
         </div>
-      )} */}
+      )}
     </div>
   );
 }
