3b505a2f72cf1e6493bc46f441a1f8f948ee58cb
mfatihfedai <mfatihfedai1@gmail.com>
2024-12-12 19:01:43 +0300

    identity control is deleted.

--- Backend/src/main/java/com/softwareProject/banksApplication/service/concretes/UserManager.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/service/concretes/UserManager.java
@@ -41,10 +41,6 @@ public class UserManager extends BaseManager<UserInfo, UserRepo, UserSaveRequest
     @Override
     public UserResponse create(UserSaveRequest request) {
         UserInfo user = mapper.saveRequestToEntity(request);
-        // Control the identity number
-//        if(!identityControl(String.valueOf(user.getIdentityNumber()))){
-//            throw new NotValidException("Please enter valid identity number.");
-//        }
         // Is user exist in the database
         Optional<UserInfo> userOptional = this.repository.findByEmailOrIdentityNumber(
                 user.getEmail(),
@@ -149,36 +145,5 @@ public class UserManager extends BaseManager<UserInfo, UserRepo, UserSaveRequest
         } while(isAvailable);
 
         return newNumber;
-    }
-
-    private boolean identityControl(String tcNo)
-    {
-        int[] sayiDizi = separate(tcNo);
-
-        if (sayiDizi!=null)
-        {
-            boolean kosul1 = (sayiDizi[0]+sayiDizi[1]+sayiDizi[2]+sayiDizi[3]+sayiDizi[4]+sayiDizi[5]+sayiDizi[6]+sayiDizi[7]+sayiDizi[8]+sayiDizi[9])%10 == sayiDizi[10];
-            boolean kosul2 = (((sayiDizi[0]+sayiDizi[2]+sayiDizi[4]+sayiDizi[6]+sayiDizi[8]) * 7 ) + ((sayiDizi[1]+sayiDizi[3]+sayiDizi[5]+sayiDizi[7]) * 9 ))%10 == sayiDizi[9];
-            boolean kosul3 = ((sayiDizi[0]+sayiDizi[2]+sayiDizi[4]+sayiDizi[6]+sayiDizi[8]) * 8 )%10 == sayiDizi[10];
-
-            return kosul1 && kosul2 && kosul3;
-        }
-        return false;
-    }
-
-    private int[] separate(String tcNo)
-    {
-        int[] numbers = new int[11];
-
-        if(tcNo == null || tcNo.length()!=11)
-        {
-            return null;
-        }
-
-        for (int i = 0; i < 11; i++)
-        {
-            numbers[i] = Integer.parseInt(tcNo.substring(i,(i+1)));
-        }
-        return numbers;
     }
 }
