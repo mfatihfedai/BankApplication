b6aae78e1e2035310f87a3dfa197837c0d428ddf
mfatihfedai <mfatihfedai1@gmail.com>
2025-01-20 14:28:07 +0300

    getLog'daki hatalar düzeltildi.

--- Backend/src/main/java/com/softwareProject/banksApplication/service/abstracts/LogService.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/service/abstracts/LogService.java
@@ -1,9 +1,11 @@
 package com.softwareProject.banksApplication.service.abstracts;
 
+import com.softwareProject.banksApplication.dto.CursorResponse;
 import com.softwareProject.banksApplication.dto.request.log.LogSaveRequest;
 import com.softwareProject.banksApplication.dto.request.log.LogUpdateRequest;
 import com.softwareProject.banksApplication.dto.response.log.LogResponse;
 import com.softwareProject.banksApplication.entity.LogInfo;
 
 public interface LogService extends IBaseService<LogInfo, LogSaveRequest, LogUpdateRequest, LogResponse>{
+    CursorResponse<LogResponse> cursorResponse(int page, int pageSize, Long id);
 }
