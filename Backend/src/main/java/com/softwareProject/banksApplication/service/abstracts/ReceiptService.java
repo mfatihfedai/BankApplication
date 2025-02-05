96a5cb6222a760abecb8edc72adcff2ae217a976
mfatihfedai <mfatihfedai1@gmail.com>
2025-02-05 08:52:36 +0300

    gereksiz kod parçacıkları temizlendi.

--- Backend/src/main/java/com/softwareProject/banksApplication/service/abstracts/ReceiptService.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/service/abstracts/ReceiptService.java
@@ -5,7 +5,6 @@ import com.softwareProject.banksApplication.dto.request.receipt.ReceiptSaveReque
 import com.softwareProject.banksApplication.dto.request.receipt.ReceiptUpdateRequest;
 import com.softwareProject.banksApplication.dto.response.receipt.ReceiptResponse;
 import com.softwareProject.banksApplication.entity.ReceiptInfo;
-import com.softwareProject.banksApplication.entity.UserInfo;
 
 public interface ReceiptService extends IBaseService<ReceiptInfo, ReceiptSaveRequest, ReceiptUpdateRequest, ReceiptResponse>{
     CursorResponse<ReceiptResponse> cursorResponse(int page, int pageSize, Long id);
