f0144c37c273b91a1759ce3eb2e591af00a66225
mfatihfedai <mfatihfedai1@gmail.com>
2025-02-19 11:11:20 +0300

    gereksiz kod parçacıkları temizlendi.

--- Backend/src/main/java/com/softwareProject/banksApplication/core/Logging/LogoutEventListener.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/core/Logging/LogoutEventListener.java
@@ -7,7 +7,6 @@ import com.softwareProject.banksApplication.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.LogoutSuccessEvent;
 import org.springframework.security.core.session.SessionDestroyedEvent;
