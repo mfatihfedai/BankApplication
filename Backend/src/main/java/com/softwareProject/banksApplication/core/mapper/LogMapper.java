ea0cf0e2c2527ea385d50223bee5f2b9d9ca1bc0
mfatihfedai <mfatihfedai1@gmail.com>
2025-02-21 00:16:41 +0300

    gereksiz kod parçacıkları temizlendi.

--- Backend/src/main/java/com/softwareProject/banksApplication/core/mapper/LogMapper.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/core/mapper/LogMapper.java
@@ -5,7 +5,6 @@ import com.softwareProject.banksApplication.dto.request.log.LogUpdateRequest;
 import com.softwareProject.banksApplication.dto.response.log.LogResponse;
 import com.softwareProject.banksApplication.entity.LogInfo;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface LogMapper extends BaseMapper<LogInfo, LogSaveRequest, LogUpdateRequest, LogResponse>{
