971336415ccf8d460b7bc8b98b3e012bceae2936
mfatihfedai <mfatihfedai1@gmail.com>
2025-03-04 09:05:14 +0300

    gereksiz kod parçacıkları temizlendi.

--- Backend/src/main/java/com/softwareProject/banksApplication/service/abstracts/UserService.java
+++ Backend/src/main/java/com/softwareProject/banksApplication/service/abstracts/UserService.java
@@ -8,7 +8,7 @@ import com.softwareProject.banksApplication.entity.UserInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+
 import java.util.Optional;
 
 public interface UserService extends IBaseService<UserInfo, UserSaveRequest, UserUpdateRequest, UserResponse> {
