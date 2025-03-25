11f89460de69cf24e0d8ade3336ccd28a104c16e
TalhaTopaloglu <talhatopaloglu9@gmail.com>
2025-03-25 03:54:44 +0300

    generalTable oluşturuldu.

--- Frontend/Rise/src/components/SignIn/GeneralComponent/LogsInfo/LogsInfo.jsx
+++ Frontend/Rise/src/components/SignIn/GeneralComponent/LogsInfo/LogsInfo.jsx
@@ -5,8 +5,13 @@ import { tr } from "date-fns/locale";
 import "./LogsInfo.css";
 import Logo from "../../../../assets/LogoNonBackground.png";
 import { useTranslation } from "react-i18next";
+import GeneralTable from "../../../General/GeneralTable";
 
 function LogsInfo() {
+  const [headers, setHeaders] = useState([]);
+  const [infos, setInfos] = useState([]);
+
+
   const [logs, setLogs] = useState([]);
   const [page, setPage] = useState(0);
   const [hasNext, setHasNext] = useState(false);
@@ -21,6 +26,15 @@ function LogsInfo() {
       console.log(response);
       const { items, hasNext } = response.data;
       setLogs(items);
+      const result = items.map(({ loginTime, logoutTime }) => ({ loginTime, logoutTime }));
+      const formattedResult = result.map((log) => ({
+        "Giris Tarihi": formatDateTime(log.loginTime),
+        "Cikis Tarihi": log.logoutTime ? formatDateTime(log.logoutTime) : "-",
+      }));  
+      setInfos(formattedResult);
+      // console.log(result);
+      // console.log(infos);
+      // console.log(formattedResult);
       setHasNext(hasNext);
       setHasPrevious(currentPage > 0);
     } catch (error) {
@@ -30,9 +44,17 @@ function LogsInfo() {
     }
   };
 
+
   useEffect(() => {
     fetchLogs(page);
   }, [page]);
+
+  // function filterData(data) {
+  //   const filteredData = data.filter((log) => log.type === "loginTime" || log.type === "logoutTime");
+  //   setInfos(filteredData);
+  // }
+
+  
 
   const formatDateTime = (dateTime) => {
     const date = new Date(dateTime);
@@ -50,6 +72,11 @@ function LogsInfo() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>{t("GirisKayitlarim")}</h1>
+
+
+      <GeneralTable data={infos}  />
+
+
       <table>
         <thead>
           <tr>
